7e66c9b503fd517ae6dadc3b9441ce75c6e1713e
madkle <mads.madsen.klepper@gmail.com>
2021-11-18 10:33:15 +0100

    added hashing

--- server.js
+++ server.js
@@ -4,13 +4,23 @@ const PORT = process.env.PORT || 8080;
 server.set("port", PORT);
 
 const blogposts = require("./modules/blogposts.js");
+const authUtils = require("./modules/auth_utils.js");
 
 // middleware ---------------------------
 server.use(express.static("public"));
 server.use(express.json());
 
 server.use(blogposts);
- 
+
+let hash = authUtils.createHash("kongolav");
+console.log(hash);
+
+let token = authUtils.createToken("jostein", 1);
+console.log(token);
+
+let payload = authUtils.verifyToken(token);
+console.log(payload);
+
 //general error handlogig----------------
 server.use(function(err, req, res, next){
 	res.status(500).json({
